026db5ecbe968056895be9598bf5de0b02e16bd7
idenrai <geniuskwon966@gmail.com>
2026-03-29 00:18:03 +0900

    fix: ALL 시장 유니버스를 30개로 통일 (US8+KR8+JP8+EU6)

--- src/utils/stockUniverse.ts
+++ src/utils/stockUniverse.ts
@@ -17,38 +17,19 @@ const US: UniverseStock[] = [
   { ticker: "HIMS",  name: "Hims & Hers Health",    market: "US" },
   { ticker: "CAVA",  name: "CAVA Group",            market: "US" },
   { ticker: "TOST",  name: "Toast",                 market: "US" },
-  { ticker: "BROS",  name: "Dutch Bros",            market: "US" },
   { ticker: "DKNG",  name: "DraftKings",            market: "US" },
-  { ticker: "ENPH",  name: "Enphase Energy",        market: "US" },
   { ticker: "UPST",  name: "Upstart Holdings",      market: "US" },
-  { ticker: "GTLB",  name: "GitLab",                market: "US" },
-  { ticker: "DOCN",  name: "DigitalOcean",          market: "US" },
-  { ticker: "IOT",   name: "Samsara",               market: "US" },
-  { ticker: "PCOR",  name: "Procore Technologies",  market: "US" },
-  { ticker: "BILL",  name: "Bill Holdings",         market: "US" },
-  { ticker: "FRSH",  name: "Freshworks",            market: "US" },
   { ticker: "SOFI",  name: "SoFi Technologies",     market: "US" },
-  { ticker: "GLBE",  name: "Global-e Online",       market: "US" },
-  { ticker: "CFLT",  name: "Confluent",             market: "US" },
-  { ticker: "MNDY",  name: "monday.com",            market: "US" },
-  { ticker: "RXRX",  name: "Recursion Pharma",      market: "US" },
 ];
 
 const KR: UniverseStock[] = [
   { ticker: "247540.KQ", name: "에코프로비엠",       market: "KR" },
-  { ticker: "086520.KQ", name: "에코프로",           market: "KR" },
   { ticker: "196170.KQ", name: "알테오젠",           market: "KR" },
   { ticker: "042700.KQ", name: "한미반도체",         market: "KR" },
-  { ticker: "022100.KS", name: "포스코DX",          market: "KR" },
   { ticker: "214150.KQ", name: "클래시스",           market: "KR" },
   { ticker: "352480.KQ", name: "씨앤씨인터내셔널",   market: "KR" },
-  { ticker: "357780.KQ", name: "솔브레인",           market: "KR" },
   { ticker: "058470.KQ", name: "리노공업",           market: "KR" },
-  { ticker: "007660.KS", name: "이수페타시스",       market: "KR" },
   { ticker: "328130.KQ", name: "루닛",              market: "KR" },
-  { ticker: "067160.KQ", name: "아프리카TV",         market: "KR" },
-  { ticker: "214450.KQ", name: "파마리서치",         market: "KR" },
-  { ticker: "041510.KQ", name: "에스엠",            market: "KR" },
   { ticker: "094280.KQ", name: "HLB",              market: "KR" },
 ];
 
@@ -58,16 +39,9 @@ const JP: UniverseStock[] = [
   { ticker: "3697.T",  name: "SHIFT",                       market: "JP" },
   { ticker: "6532.T",  name: "ベイカレント (BayCurrent)",     market: "JP" },
   { ticker: "6526.T",  name: "ソシオネクスト (Socionext)",     market: "JP" },
-  { ticker: "3769.T",  name: "GMOペイメントG",               market: "JP" },
   { ticker: "9552.T",  name: "M&A総合研究所",                market: "JP" },
   { ticker: "3923.T",  name: "ラクス (RAKUS)",               market: "JP" },
-  { ticker: "3994.T",  name: "マネーフォワード (MoneyForward)", market: "JP" },
-  { ticker: "6254.T",  name: "野村マイクロサイエンス",         market: "JP" },
-  { ticker: "4816.T",  name: "東映アニメーション",             market: "JP" },
-  { ticker: "2158.T",  name: "FRONTEO",                     market: "JP" },
-  { ticker: "7342.T",  name: "ウェルスナビ (WealthNavi)",     market: "JP" },
   { ticker: "6920.T",  name: "レーザーテック",                market: "JP" },
-  { ticker: "4420.T",  name: "イーソル (eSOL)",              market: "JP" },
 ];
 
 const EU: UniverseStock[] = [
@@ -76,11 +50,7 @@ const EU: UniverseStock[] = [
   { ticker: "HAG.DE",    name: "Hensoldt",              market: "EU" },
   { ticker: "RHM.DE",    name: "Rheinmetall",           market: "EU" },
   { ticker: "DSY.PA",    name: "Dassault Systèmes",     market: "EU" },
-  { ticker: "CAP.PA",    name: "Capgemini",             market: "EU" },
   { ticker: "NEXI.MI",   name: "Nexi",                  market: "EU" },
-  { ticker: "PRX.AS",    name: "Prosus",                market: "EU" },
-  { ticker: "SRT3.DE",   name: "Sartorius (Pref.)",     market: "EU" },
-  { ticker: "DHL.DE",    name: "DHL Group",             market: "EU" },
 ];
 
 export const STOCK_UNIVERSE: Record<Market, UniverseStock[]> = {
